55eb1aeb7f2613c238d8ca2c219f65d185135438
invalidunits <invalidunits@gmail.com>
2025-05-12 22:25:32 -0400

    Jolly Player Icons (still a bit broken)

--- Menu/RainMeadow.MenuHooks.cs
+++ Menu/RainMeadow.MenuHooks.cs
@@ -72,6 +72,7 @@ namespace RainMeadow
         {
             if (self is StoryOnlineMenu sOM)
             {
+                if (ModManager.JollyCoop) return; // jolly has it's own coloring system. 
                 if (sOM.colorInterface == null)
                 {
                     sOM.SetupSelectableSlugcats();
